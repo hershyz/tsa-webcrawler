dcbb1694fd7924abcc63b52deaab2b30e2ab63c5
Hrishikesh Bagalkote <hrishikeshbagalkote@Hrishikeshs-MacBook-Pro.local>
2022-12-22 00:04:30 -0500

    file io

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@
 import url_fetcher
 
 # test
-url_fetcher.update_urls(1000)
+url_fetcher.update_urls(1000, 0)
 for url in url_fetcher.urls:
     print(url)
 print('current length: ' + str(len(url_fetcher.urls)))
--- backend/url_fetcher.py
+++ backend/url_fetcher.py
@@ -1,33 +1,43 @@
 import requests
 import re
 
-urls = ['https://www.theverge.com']
-last_index = [-1]
+urls = []
 
 def extract(res):
     regex = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
     url = re.findall(regex,res)
     return [x[0] for x in url]
 
-def update_urls(n): # n = number of urls to add
+def update_urls(n, index): # n = number of urls to add
 
     desired_length = len(urls) + n
 
+    # read
+    f = open('urls.txt')
+    lines = f.readlines()
+    for line in lines:
+        line = line.replace('\n', '')
+        urls.append(line)
+
     # update
-    index = last_index[0] + 1
     while True:
 
         curr = urls[index]
         index += 1
         try:
             res = requests.get(curr).text
+            found = extract(res)
+            for url in found:
+                if '.jpeg' not in url and '.jpg' not in url and '.svg' not in url and '.png' not in url and '.gif' not in url: # don't crawl image files
+                    urls.append(url)
         except:
             continue
-        found = extract(res)
-        for url in found:
-            if '.jpeg' not in url and '.jpg' not in url and '.svg' not in url and '.png' not in url: # don't crawl image files
-                urls.append(url)
 
         if index == len(urls) or len(urls) >= desired_length:
-            last_index[0] = index
             break
+    
+    # write
+    f.close()
+    f = open('urls.txt', 'w')
+    for url in urls:
+        f.write(url + '\n')
