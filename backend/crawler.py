fe08cb0d3dd4a2df95e4943461ee35200c2a9ca1
Hrishikesh Bagalkote <hrishikeshbagalkote@Hrishikeshs-MacBook-Pro.local>
2022-12-26 13:46:17 -0500

    optimizations

--- backend/crawler.py
+++ backend/crawler.py
@@ -3,7 +3,7 @@ import time
 
 index = 0
 while True:
-    url_fetcher.update_urls(100, index)
+    url_fetcher.update_urls(index)
     index += 1
     print('cycles: ' + str(index))
     time.sleep(5)
--- backend/url_fetcher.py
+++ backend/url_fetcher.py
@@ -2,7 +2,8 @@ import requests
 import re
 
 urls = []
-forbidden = ['.jpeg', '.jpg', '.svg', '.png', '.gif', '.css', '.js', '.mp4', '.mp3', '.wav', '.mov', '.woff'] # don't crawl image, movies, audio, font, or code files
+urls_set = set()
+forbidden = ['.jpeg', '.jpg', '.svg', '.png', '.gif', '.css', '.js', '.mp4', '.mp3', '.wav', '.mov', '.woff'] # don't crawl image, movies, audio, font, javascript, or css files
 
 def extract(res):
     regex = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
@@ -10,14 +11,14 @@ def extract(res):
     return [x[0] for x in url]
 
 def verify(url):
+    if url in urls_set:
+        return False
     for extension in forbidden:
         if extension in url:
             return False
     return True
 
-def update_urls(n, index): # n = number of urls to add
-
-    desired_length = len(urls) + n
+def update_urls(index): # n = number of urls to add
 
     # read
     f = open('urls.txt')
@@ -25,27 +26,23 @@ def update_urls(n, index): # n = number of urls to add
     for line in lines:
         line = line.replace('\n', '')
         urls.append(line)
+        urls_set.add(line)
 
     # update
-    while True:
-
-        curr = urls[index]
-        index += 1
-        try:
-            res = requests.get(curr).text
-            found = extract(res)
-            for url in found:
-                if verify(url):
-                    urls.append(url)
-
-        except:
-            continue
-
-        if index == len(urls) or len(urls) >= desired_length:
-            break
+    curr = urls[index]
+    try:
+        res = requests.get(curr).text
+        found = extract(res)
+        for url in found:
+            if verify(url):
+                urls.append(url)
+                urls_set.add(url)
+    except:
+        pass
     
     # write
     f.close()
     f = open('urls.txt', 'w')
     for url in urls:
         f.write(url + '\n')
+    urls.clear()
