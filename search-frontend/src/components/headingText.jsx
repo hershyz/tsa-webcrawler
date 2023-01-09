4de194b7842142c56ed5014d3a2163ee142b8655
Hrishikesh Bagalkote <hrishikeshbagalkote@Hrishikeshs-MacBook-Pro.local>
2023-01-08 23:29:15 -0500

    init frontend

---
+++ search-frontend/src/components/headingText.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class headingText extends Component {
+    render() {
+        return (
+            <div style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
+                <h1>TSA WebCrawler Search Engine</h1>
+            </div>
+        )
+    }
+}
+
+export default headingText;
---
+++ search-frontend/src/components/searchField.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class searchField extends Component {
+    render() {
+        return (
+            <div style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
+                <input placeholder="Query..." className="form-control" style={{width: "600px"}}/>
+                <button type="button" className="btn btn-dark m-2">Search</button>
+            </div>
+        )
+    }
+}
+
+export default searchField;
