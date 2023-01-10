535cf63d578820fe492677470c05324c8b44c301
Hrishikesh Bagalkote <hrishikeshbagalkote@Hrishikeshs-MacBook-Pro.local>
2023-01-10 08:49:38 -0500

    results frontend

---
+++ search-frontend/src/components/results.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class searchField extends Component {
+
+    function 
+
+    render() {
+        return (
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <pre>
+                    dis is a placeholder
+                </pre>
+            </div>
+        )
+    }
+}
+
+export default searchField;
--- search-frontend/src/index.js
+++ search-frontend/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import SearchField from './components/searchField';
 import HeadingText from './components/headingText';
+import Results from './components/results'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <HeadingText/>
   <SearchField/>
+  <Results/>
   </React.StrictMode>
 );
