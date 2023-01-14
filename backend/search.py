f13f9bc2219e48421e598d0ebea73c485b6a414c
Hrishikesh Bagalkote <hrishikeshbagalkote@Hrishikeshs-MacBook-Pro.local>
2023-01-14 00:31:12 -0500

    make search function on the backend better

--- backend/search.py
+++ backend/search.py
@@ -29,5 +29,13 @@ def search(text):
     res = sorted(res.items(), key=lambda x:x[1], reverse=True) # turns the dict into an array of tuples
     urls = []
     for element in res:
-        urls.append(element[0])
+        url = element[0]
+        freq = element[1]
+
+        # some basic logic to get rid of totally irrelevant results
+        if len(words) == 1:
+            urls.append(url)
+        if len(words) > 1 and freq > 1:
+            urls.append(url)
+
     return urls
